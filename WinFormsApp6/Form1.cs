75d31f644a57b3ad3860d944e6fc878e69be5f67
hasan.baysal <hasan.baysal@ANK3-YZLMORT-08>
2024-02-01 15:49:36 +0300

    uyasdasd

---
+++ WinFormsApp6/Form1.cs
@@ -0,0 +1,64 @@
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+
+        private string defaultConfigValues = 
+            "FormText|form1" + Environment.NewLine +
+            "FormBackColor|-123123" +Environment.NewLine + 
+            "FormForeColor|-123123";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (File.Exists("config.txt"))
+            {
+                var data = File.ReadAllLines("config.txt");
+                SetConfigurations(data);
+            }
+            else
+            {
+                
+
+                File.WriteAllText("config.txt", defaultConfigValues);
+
+               var data = File.ReadAllLines("config.txt");
+                SetConfigurations(data);
+            }
+
+        }
+        private void SetConfigurations(string []configs)
+        {
+
+            try
+            {
+                this.Text = configs[0].Split('|')[1];
+                this.BackColor = Color.FromArgb(int.Parse(configs[1].Split('|')[1]));
+                this.ForeColor = Color.FromArgb(int.Parse(configs[2].Split('|')[1]));
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("bir hata meydana geldi varsayýlan ayarlar yükleniyor ");
+
+
+                var arr = defaultConfigValues.Split(Environment.NewLine);
+
+
+                this.Text = arr[0].Split('|')[1];
+                this.BackColor = Color.FromArgb(int.Parse(arr[1].Split('|')[1]));
+                this.ForeColor = Color.FromArgb(int.Parse(arr[2].Split('|')[1]));
+
+
+            }
+
+
+
+        } 
+    }
+}
